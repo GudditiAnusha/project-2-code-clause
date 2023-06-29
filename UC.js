10ea9c20edb13dbf1261b166e5d9b050f4b54618
GudditiAnusha <112812565+GudditiAnusha@users.noreply.github.com>
2023-06-29 21:48:41 +0530

    UC.js

--- UC.js
+++ UC.js
@@ -1,53 +1,80 @@
-var list = document.getElementsByTagName("LI");
-var i;
-for(i=0;i<list.length;i++) 
-{
-  var span = document.createElement("SPAN");
-  var text = document.createTextNode("\u00D7");
-  span.className = "cross";
-  span.appendChild(text);
-  list[i].appendChild(span);
+let feet    =document.getElementById("feet");
+let meter   =document.getElementById("meter");
+let inch    =document.getElementById("inches");
+let cm      =document.getElementById("cm");
+let yard    =document.getElementById("yards");
+let km      =document.getElementById("km");
+let mile    =document.getElementById("miles");
+
+function feetToOther(val){
+    meter.value =  val/3.2808; 
+    inch.value =  val*12;   
+    cm.value    =  val/0.032808; 
+    yard.value  =  val*0.33333;  
+    km.value    =  val/3280.8; 
+    mile.value  =  val*0.00018939;       
 }
-
-var cross = document.getElementsByClassName("cross");
-var i;
-for(i=0;i<cross.length;i++)
-{
-  cross[i].onclick = function()
-  {
-     var div = this.parentElement;
-     div.style.display = "none";
-  }
+function meterToOther(val){
+    feet.value = val*3.2808;
+    inch.value = val*39.370;  
+    cm.value   = val/0.01;
+    yard.value = val*1.0936; 
+    km.value   = val/1000;
+    mile.value = val*0.00062137;
 }
+function inchesToOther(val){
+    feet.value = val*0.083333;
+    meter.value = val/39.370;  
+    cm.value = val/0.39370;
+    yard.value = val*0.027778; 
+    km.value = val/39370;
+    mile.value = val*0.000015783;
+}
+function cmToOther(val){
+    feet.value = val*0.032808;
+    meter.value = val/100;  
+    inch.value = val*0.39370;
+    yard.value = val*0.010936; 
+    km.value = val/100000 ;
+    mile.value = val*0.0000062137;
+}
+function yardsToOther(val){
+    feet.value = val*3;
+    inch.value = val*36;  
+    cm.value = val/0.010936;
+    meter.value = val/1.0936; 
+    km.value = val/1093.6;
+    mile.value = val*0.00056818;
+}
+function kmToOther(val){
+    feet.value = val*3280.8;
+    inch.value = val*39370;  
+    cm.value = val*100000;
+    yard.value = val*1093.6; 
+    meter.value = val*1000;
+    mile.value = val*0.62137;
+}
+function milesToOther(val){
+    feet.value = val*5280;
+    inch.value = val*63360;  
+    cm.value = val/0.0000062137;
+    yard.value = val*1760; 
+    km.value = val/0.62137;
+    meter.value = val/0.00062137;
 
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev)
-{
-  if (ev.target.tagName === 'LI') 
-  {
-    ev.target.classList.toggle('checked');
-  }
-},false);
+}
+// *********************//
 
-function newElement() {
-  var li = document.createElement("li");
-  var ivalue = document.getElementById("Inputs").value;
-  var t = document.createTextNode(ivalue);
-  li.appendChild(t);
-  document.getElementById("UL").appendChild(li);
-  document.getElementById("Inputs").value = "";
 
-  var span = document.createElement("SPAN");
-  var text = document.createTextNode("\u00D7");
-  span.className = "cross";
-  span.appendChild(text);
-  li.appendChild(span);
 
-  for(i=0;i<cross.length;i++)
-  {
-    cross[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+function convertToOthers(convertFrom,value){    
+    switch(convertFrom){
+        case "feet" : feetToOther (parseFloat(value)); break;
+        case "meter": meterToOther(parseFloat(value)); break;
+        case "inch" : inchesToOther(parseFloat(value)); break;
+        case "cm"   : cmToOther(parseFloat(value)); break;
+        case "yard" : yardsToOther (parseFloat(value)); break;
+        case "km"   : kmToOther (parseFloat(value)); break;
+        case "mile" : milesToOther(parseFloat(value)); break;
     }
-  }
 }
